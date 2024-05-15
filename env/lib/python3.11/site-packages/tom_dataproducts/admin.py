62d69de75b336795a077eec13b686f5aca034552
JA PAICE <stmg31@durham.ac.uk>
2024-05-15 16:43:30 +0100

    Deleted env directory

--- env/lib/python3.11/site-packages/tom_dataproducts/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from tom_dataproducts.models import DataProduct, DataProductGroup, ReducedDatum
-
-admin.site.register(DataProduct)
-admin.site.register(DataProductGroup)
-admin.site.register(ReducedDatum)
