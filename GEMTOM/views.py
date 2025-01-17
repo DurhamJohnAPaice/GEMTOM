c588fe91454e66e948f0e298835ff02135622c1f
JohnAPaice <johnapaice@gmail.com>
2025-01-17 15:48:08 +0000

    Bugfixing...

--- GEMTOM/views.py
+++ GEMTOM/views.py
@@ -1967,7 +1967,7 @@ def NightView(request, obs_date):
         # df_orphans.std_min = df_orphans.std_min.fillna(value=np.nan)
         df_orphans.std_min = df_orphans.std_min.replace('', np.nan)
         # df_orphans.std_frc = df_orphans.std_min.fillna(value=np.nan)
-        df_orphans.std_frc = df_orphans.std_min.replace('', np.nan)
+        df_orphans.std_frc = df_orphans.std_frc.replace('', np.nan)
         # print(df_orphans['std_min'][2:5])
         # print(type(df_orphans['std_min'][3]))
         # aaa_orphans_std_min = df_orphans.std_min
