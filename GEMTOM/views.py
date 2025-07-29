acdb2b40d0ccfb7a32bab074cdd49958496cd107
JohnAPaice <johnapaice@gmail.com>
2025-07-29 15:07:17 +0100

    Updated colour

--- GEMTOM/views.py
+++ GEMTOM/views.py
@@ -2153,7 +2153,7 @@ def NightView(request, obs_date):
         # print(df_orphans.std_min)
 
         # real_bogus_color = df_orphans["real_bogus_probabilities"]
-        mediumaquamarine_rgb = tuple(int("0ACC8D"[i:i+2], 16) for i in (0, 2, 4))
+        mediumaquamarine_rgb = tuple(int("099C6C"[i:i+2], 16) for i in (0, 2, 4))
         darkorange_rgb = tuple(int("E8840C"[i:i+2], 16) for i in (0, 2, 4))
         lightgrey_rgb = tuple(int("D3D3D3"[i:i+2], 16) for i in (0, 2, 4))
         # real_bogus_blue = df_orphans["real_bogus_probabilities"]*(mediumaquamarine_rgb[2]-lightgrey_rgb[2])+lightgrey_rgb[2]
